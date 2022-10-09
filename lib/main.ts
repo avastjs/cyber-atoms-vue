574ab4df5bb074bab990a969200a99894f5d0ebc
MBJ <thiswallz@gmail.com>
2022-10-09 14:03:53 -0300

    expose ads

--- lib/main.ts
+++ lib/main.ts
@@ -1,8 +1,10 @@
 import './style.css'; 
 import FancyNavbar from "./components/FancyNavbar.vue"
+import XAds  from "./components/atoms/XAds.vue"
 
 export default {
   install: (app: any) => {
     app.component("FancyNavbar", FancyNavbar)
+    app.component("XAds", XAds)
   }
 }
